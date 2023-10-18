c6698e12e5c5408e24f4e14ddcf4eeba5fd08ba6
S <s@winnipeg5.de>
2023-10-18 11:13:21 +0200

    externalized common node functions

--- src/lib/node-base.ts
+++ src/lib/node-base.ts
@@ -1,5 +1,6 @@
-import { INodeConfig, NodeAPI, NodeStatusFill, StatusLevel } from '../types'
+import { INodeConfig, NodeAPI, StatusLevel } from '../types'
 import { AbstractRedNode } from './abstract-node'
+import { NSPanelNodeUtils } from './nspanel-node-utils'
 
 export class NodeBase<TConfig extends INodeConfig, TCreds extends {} = {}> extends AbstractRedNode<TConfig, TCreds> {
     constructor(config: TConfig, RED: NodeAPI) {
@@ -7,26 +8,9 @@ export class NodeBase<TConfig extends INodeConfig, TCreds extends {} = {}> exten
     }
 
     protected setNodeStatus(statusLevel: StatusLevel, msg: string): void {
-        var statusFill: NodeStatusFill
+        const nodeRedStatus = NSPanelNodeUtils.createNodeStatus(statusLevel, msg)
 
-        switch (statusLevel) {
-            case 'error':
-                statusFill = 'red'
-                break
-            case 'warn':
-                statusFill = 'yellow'
-                break
-            case 'info':
-                statusFill = 'blue'
-                break
-            case 'success':
-                statusFill = 'green'
-                break
-            default:
-                statusFill = 'grey'
-        }
-
-        this.status({ fill: statusFill, shape: 'dot', text: msg })
+        this.status(nodeRedStatus)
     }
 
     protected clearNodeStatus() {
---
+++ src/lib/nspanel-node-utils.ts
@@ -0,0 +1,31 @@
+import { NodeStatus, NodeStatusFill, StatusLevel } from "../types"
+
+export class NSPanelNodeUtils {
+
+    public static createNodeStatus(statusLevel: StatusLevel, msg: string)
+        : NodeStatus | null {
+
+        var statusFill: NodeStatusFill
+
+        switch (statusLevel) {
+            case 'error':
+                statusFill = 'red'
+                break
+            case 'warn':
+                statusFill = 'yellow'
+                break
+            case 'info':
+                statusFill = 'blue'
+                break
+            case 'success':
+                statusFill = 'green'
+                break
+            default:
+                statusFill = 'grey'
+        }
+
+        return { fill: statusFill, shape: 'dot', text: msg }
+    }
+
+
+}
