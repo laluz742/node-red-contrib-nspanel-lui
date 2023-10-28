9f215b36397b60076b059031246531cd88cf594d
laluz742 <laluz742@icloud.com>
2023-10-28 08:52:02 +0200

    type check

--- src/lib/nspanel-message-utils.ts
+++ src/lib/nspanel-message-utils.ts
@@ -1,6 +1,7 @@
 import { DEFAULT_LUI_COLOR } from './nspanel-constants'
 import { NSPanelColorUtils } from './nspanel-colorutils'
 import { BuzzerCommandParams, CommandData, PageEntityData, StatusItemData, SwitchCommandParams } from '../types/types'
+import { NSPanelUtils } from './nspanel-utils'
 
 const DEFAULT_STATUS: StatusItemData = { icon: undefined, iconColor: DEFAULT_LUI_COLOR, text: undefined }
 const DEFAULT_DATA: PageEntityData = { icon: undefined, iconColor: DEFAULT_LUI_COLOR, text: undefined }
@@ -34,13 +35,18 @@ export class NSPanelMessageUtils {
     public static convertToEntityItemData(input: any, defaultData: PageEntityData = DEFAULT_DATA): PageEntityData {
         const result: PageEntityData = { ...defaultData }
 
-        // TODO: intNameEntity
-        result.text = NSPanelMessageUtils.hasProperty(input, 'text', true) ? input['text'] : null // TODO: could be anything else but string
-        result.value = NSPanelMessageUtils.hasProperty(input, 'value', true) ? input['value'] : null // TODO: could be anything else but string
-        result.icon = NSPanelMessageUtils.hasProperty(input, 'icon', true) ? input['icon'] : null // TODO: could be anything else but string
-        result.iconColor = NSPanelMessageUtils.hasProperty(input, 'iconColor')
+        const dText: any = NSPanelMessageUtils.hasProperty(input, 'text', true) ? input['text'] : null
+        const dValue: any = NSPanelMessageUtils.hasProperty(input, 'value', true) ? input['value'] : null
+        const dIcon: any = NSPanelMessageUtils.hasProperty(input, 'icon', true) ? input['icon'] : null
+        const dIconColor: any = NSPanelMessageUtils.hasProperty(input, 'iconColor')
             ? NSPanelColorUtils.toHmiIconColor(input['iconColor'])
             : DEFAULT_LUI_COLOR
+
+        // TODO: intNameEntity
+        result.text = NSPanelUtils.isString(dText) ? dText : null
+        result.value = NSPanelUtils.isString(dValue) ? dValue : null
+        result.icon = NSPanelUtils.isString(dIcon) ? dIcon : null
+        result.iconColor = dIconColor
 
         return result
     }
