ddc8b80c1a1e00f26437305c923d8f6a9c09aa82
laluz742 <laluz742@icloud.com>
2023-12-17 15:00:58 +0100

    removed debug code

--- src/lib/nspanel-controller.ts
+++ src/lib/nspanel-controller.ts
@@ -375,7 +375,7 @@ export class NSPanelController extends nEvents.EventEmitter implements IPanelCon
                 break
 
             default:
-                log.debug(`UNCATCHED onEvent default ${JSON.stringify(eventArgs)}`)
+                // log.debug(`UNCATCHED onEvent default ${JSON.stringify(eventArgs)}`)
                 // dispatch to active page
                 this.notifyCurrentPageOfEvent('input', eventArgs)
         }
@@ -385,8 +385,6 @@ export class NSPanelController extends nEvents.EventEmitter implements IPanelCon
         if (msg.type === 'fw') {
             const fwEventArgs: FirmwareEventArgs = msg as FirmwareEventArgs
             this._panelUpdater?.onFirmwareEvent(fwEventArgs)
-        } else {
-            log.debug(`UNCATCHED msg ${JSON.stringify(msg)}`)
         }
     }
 
