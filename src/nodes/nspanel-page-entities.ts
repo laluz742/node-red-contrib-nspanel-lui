73d889426c884d51f862417077c50227a100c186
laluz742 <laluz742@icloud.com>
2024-01-18 13:52:24 +0100

    US-Panel type #148

--- src/nodes/nspanel-page-entities.ts
+++ src/nodes/nspanel-page-entities.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-import-module-exports */
 import { EntitiesPageNode } from '../lib/entities-page-node'
 import { STR_PAGE_TYPE_CARD_ENTITIES } from '../lib/nspanel-constants'
-import { EntityBasedPageConfig } from '../types/types'
+import { EntityBasedPageConfig, IPanelNodeEx } from '../types/types'
 
 interface PageEntitiesConfig extends EntityBasedPageConfig {}
 
@@ -11,6 +11,14 @@ module.exports = (RED) => {
     class PageEntitiesNode extends EntitiesPageNode<PageEntitiesConfig> {
         constructor(config: PageEntitiesConfig) {
             super(config, RED, { pageType: STR_PAGE_TYPE_CARD_ENTITIES, maxEntities: MAX_ENTITIES })
+
+            const panelNode = (<unknown>RED.nodes.getNode(config.nsPanel)) as IPanelNodeEx
+            if (panelNode !== null) {
+                const panelType = panelNode.getPanelConfig()?.panel?.panelType
+                if (panelType === 'us-p') {
+                    super.setMaxEntities(6)
+                }
+            }
         }
     }
     RED.nodes.registerType('nspanel-page-entities', PageEntitiesNode)
